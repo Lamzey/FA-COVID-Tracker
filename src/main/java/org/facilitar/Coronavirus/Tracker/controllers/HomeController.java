a796598ee82f8408cd3a67eeed3e35ce1bb15d28
Lamzey Aribido <48647671+Lamzey@users.noreply.github.com>
2020-05-20 13:04:26 +0100

    First Commit

---
+++ src/main/java/org/facilitar/Coronavirus/Tracker/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package org.facilitar.Coronavirus.Tracker.controllers;
+
+import org.facilitar.Coronavirus.Tracker.models.LocationStats;
+import org.facilitar.Coronavirus.Tracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        final List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        
+        return "home";
+    }
+}
